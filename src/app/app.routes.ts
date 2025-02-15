924799074277b9c3af8b76c1b0a3dd65ac0d03af
Jose <pedrojcunha7@hotmail.com>
2025-02-15 12:00:41 +0100

    chore: setup login page and card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: LoginPageComponent
+    }
+
+];
---
+++ src/app/components/login-card/login-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-card',
+  imports: [],
+  templateUrl: './login-card.component.html',
+  styleUrl: './login-card.component.css'
+})
+export class LoginCardComponent {
+
+}
